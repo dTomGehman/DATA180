029dcd5a44da1145eba4715ddffc10ed6bde291f
dTomGehman <143711361+dTomGehman@users.noreply.github.com>
2023-10-09 13:17:03 -0400

    in class practice with dplyr

--- dplyr practice/dplry.R
+++ dplyr practice/dplry.R
@@ -26,9 +26,10 @@ head(Cars93_Ex4)
 
 Cars93_Ex7<-select(filter(Cars93, Type %in% c("Sporty","Compact") & Horsepower >=120),Model,Type,EngineSize,Cylinders,Horsepower,MPG.highway)
 
-Cars93_Ex7<-select(filter(Cars93, Type %in% c("Sporty","Compact") & Horsepower >=120),
+Cars93_Ex7<-select(filter(Cars93, Type %in% c("Van","Small") & Horsepower >=120),
             Model,Type,EngineSize,Cylinders,Horsepower,MPG.highway)
 head(Cars93_Ex7)
+
 
 # Mutate function.
 Cars93_Ex9<-select(mutate(Cars93,HPpLiter=Horsepower/EngineSize),Model,Type,EngineSize,Cylinders,Horsepower,MPG.highway,HPpLiter)
@@ -38,7 +39,7 @@ Cars93_Ex9<-select(mutate(Cars93,HPpLiter=Horsepower/EngineSize),Model,Type,Engi
 Cars93_Ex9<-Cars93 %>%mutate(HPpLiter=Horsepower/EngineSize) %>%select(Model,Type,EngineSize,Cylinders,Horsepower,MPG.highway,HPpLiter)
 
 #Using the arrange function to sort your dataframe
-Cars93_Ex10<-arrange(Cars93_Ex9,Cylinders,desc(Horsepower))
+Cars93_Ex10<-arrange(Cars93_Ex9,Cylinders, desc(Horsepower))
 head(Cars93_Ex10)
 
 
