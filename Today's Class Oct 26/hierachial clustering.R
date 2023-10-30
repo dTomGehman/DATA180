1ae205925b661ada907c7b269ebc61e1c5410850
dTomGehman <143711361+dTomGehman@users.noreply.github.com>
2023-10-30 16:53:28 -0400

    In class practice

--- Today's Class Oct 26/hierachial clustering.R
+++ Today's Class Oct 26/hierachial clustering.R
@@ -18,7 +18,7 @@ plot(Cluster_Ex_HC,cex=.75,xlab="")
 
 #Find clusters in the data
 Cluster_Ex_HC<-hclust(dist(Cluster_Ex))
-cutree(Cluster_Ex_HC,k=3)
+cutree(Cluster_Ex_HC,k=3) #cut dendrogram by no of clusters (3)
 
 # Find different number of clusters within the data
 Cluster_Ex<-data.frame(Cluster_Ex,cutree(Cluster_Ex_HC,k=c(3,4,5)))
@@ -26,7 +26,7 @@ names(Cluster_Ex)
 names(Cluster_Ex)<-c("X","X1","X2","k_3","k_4","k_5")
 head(Cluster_Ex)
 
-Cluster_Ex<-read.csv("D:/Dickinson College Teaching/DATA 180/data/Cluster_Ex_clusters.csv")
+Cluster_Ex<-read.csv("C:/Users/gehmand/OneDrive - Carlisle Area School District/Desktop/Data 180 dTomGehman/DATA180/Today's Class Oct 26/Cluster_Ex_clusters.csv")
 
 #plot the clusters
 plot(X2~X1,data=Cluster_Ex,xlim=c(0,4),ylim=c(0,4),cex.axis=1.3,cex.lab=1.2,cex=1.2,pch=15+k_3,col=k_3)
@@ -40,3 +40,7 @@ plot(Cluster_Ex_HC,cex=1.25,xlab="")
 
 
 plot(Cluster_Ex,hang=-1,main="Main",sub="Sub",lwd=2,cex=.8,xlab="",frame.plot=T)
+
+library(ggdendro)
+
+
